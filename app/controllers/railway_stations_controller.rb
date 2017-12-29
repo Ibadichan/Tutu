10295bda2f91700ff6ee0ca7ee4d00332b77e057
Ivan Badichan <badican01117@gmail.com>
2017-12-29 17:30:13 +0100

    Create and Show railway station

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -1,6 +1,28 @@
 class RailwayStationsController < ApplicationController
+  before_action :set_railway_station, only: :show
+
   def index
     @railway_stations = RailwayStation.order(id: :asc).page params[:page]
     respond_with @railway_stations
+  end
+
+  def show; end
+
+  def new
+    respond_with @railway_station = RailwayStation.new
+  end
+
+  def create
+    respond_with @railway_station = RailwayStation.create(railway_station_params)
+  end
+
+  private
+
+  def set_railway_station
+    @railway_station = RailwayStation.find(params[:id])
+  end
+
+  def railway_station_params
+    params.require(:railway_station).permit(:title)
   end
 end
--- spec/factories/railway_stations.rb
+++ spec/factories/railway_stations.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :railway_station do
     sequence(:title) { |n| "my_station_title_#{n}" }
+    factory :invalid_railway_station do
+      title nil
+    end
   end
 end
