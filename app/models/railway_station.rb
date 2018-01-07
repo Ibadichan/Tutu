32c0901b4f51055151b8c23a383eeb3301fa500f
Ivan Badichan <badican01117@gmail.com>
2018-01-07 20:03:05 +0100

    Station and Train association

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,5 @@ class RailwayStation < ApplicationRecord
 
   has_many :railway_stations_routes, dependent: :destroy
   has_many :routes, through: :railway_stations_routes
+  has_many :trains
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -3,5 +3,7 @@ class Train < ApplicationRecord
   validates :number, presence: true
 
   belongs_to :route, optional: true
+  belongs_to :current_station, class_name: 'RailwayStation',
+                               foreign_key: :railway_station_id, optional: true
   has_many :tickets
 end
